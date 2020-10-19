5c5fd97577d1db7e4fbd463f464f7bf9bf8d3fea
Kirugan <paltsev.kir@gmail.com>
2020-10-19 22:35:16 +0300

    'Initial'

---
+++ cmake-build-debug/CMakeFiles/otus_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/otus_cpp.dir/main.cpp.obj"
+  "otus_cpp.exe"
+  "otus_cpp.exe.manifest"
+  "otus_cpp.lib"
+  "otus_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/otus_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
